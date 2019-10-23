9696b6484c37bcf460bb865aec44562cf29ddfd3
HENRY\Henry <hkeane@usc.edu>
2019-10-23 15:55:07 -0400

    Testing new socket io
    Copy/pasted code from nkzawa to see how it works

---
+++ app/src/main/java/com/example/dndeploy/DraftActivity.kt
@@ -0,0 +1,52 @@
+package com.example.dndeploy
+
+import android.app.PendingIntent.getActivity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.github.nkzawa.socketio.client.IO
+import com.github.nkzawa.emitter.Emitter
+import org.json.JSONException
+import org.json.JSONObject
+
+
+class DraftActivity : AppCompatActivity() {
+    var mSocket =  IO.socket("https://851e8f58.ngrok.io")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_draft)
+
+        mSocket.connect()
+        mSocket.on("chat message", chatMessage)
+
+        val socketToConsoleButton = findViewById<Button>(R.id.socketToConsoleButton)
+
+        socketToConsoleButton.setOnClickListener{
+            val messageType = "chat message"
+            val messageContents = "sample text"
+            mSocket.emit(messageType,messageContents)
+        }
+    }
+//    fun chatMessage():Emitter.Listener{
+//
+//    }
+    private val chatMessage = object : Emitter.Listener {
+    override fun call(vararg args: Any) {
+            (this@DraftActivity).runOnUiThread(Runnable {
+                val data = args[0] as JSONObject
+                val msg: String
+                try {
+                    msg = data.getString("msg")
+                } catch (e: JSONException) {
+                    print(e)
+                    return@Runnable
+                }
+
+                val returnValTextView = findViewById<TextView>(R.id.returnValTextView)
+                returnValTextView.text = msg
+            })
+        }
+    }
+}
